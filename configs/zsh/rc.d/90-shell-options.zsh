0b7dc56e30bef1143db4794e8979c7c672e23683
Nastem <pengsteam16@gmail.com>
2026-06-01 00:07:21 +0800

    feat(zsh): add kitty scrollback command editing

--- configs/zsh/rc.d/90-shell-options.zsh
+++ configs/zsh/rc.d/90-shell-options.zsh
@@ -1,6 +1,18 @@
 # Keybindings (Emacs mode).
 bindkey -e
 
+# Edit the current command line with kitty-scrollback.nvim.
+export KITTY_SCROLLBACK_NVIM_EDIT_ARGS='--nvim-args -n'
+autoload -Uz edit-command-line
+zle -N edit-command-line
+kitty_scrollback_edit_command_line() {
+    local VISUAL='/home/nastem/.local/share/nvim/lazy/kitty-scrollback.nvim/scripts/edit_command_line.sh'
+    zle edit-command-line
+    zle kill-whole-line
+}
+zle -N kitty_scrollback_edit_command_line
+bindkey '^x^e' kitty_scrollback_edit_command_line
+
 # Help command support.
 unalias run-help 2>/dev/null || true
 autoload -Uz run-help
